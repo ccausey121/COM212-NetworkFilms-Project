06b03e9bc46582a43449cf1ad67c64ef00c84fd6
ccausey121 <ccausey@conncoll.edu>
2026-04-20 09:52:24 -0400

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,54 @@
+//Customer:
+//A class with identification and methods to change view info
+//Caleb Causey
+
+public class Customer
+{
+	private String name;
+	private String email;
+	private int ssn;
+	private Node next;
+	
+	public Node(String name0, String email0, int ssn0) 
+	{
+		name = name0;
+		email = email0;
+		card = ssn0;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name0) 
+	{
+		name = name0;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setEmail(String email0) 
+	{
+		email = email0;
+	}
+	public void setSSN(int ssn0) 
+	{
+		ssn = ssn0;
+	}
+	public int getSSN() 
+	{
+		return ssn;
+	}
+	public void setNext(Node next0)
+	{
+		next = next0;
+	}
+	public Node getNext()
+	{
+		return next;
+	}
+	public int getKey()
+	{
+		return card %10000;
+	}
+}
