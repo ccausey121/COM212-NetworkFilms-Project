adde25eecd20ec1d78982f258771aa1ed941d92e
ccausey121 <ccausey@conncoll.edu>
2026-04-25 15:00:48 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,26 +3,34 @@
 
 public class Customer
 {
+	//For Identification
 	private String name;
 	private String email;
 	private int card; //going to be 4 digits 
-
+	
+	//For BST
 	private Customer left;
 	private Customer right;
 	
-	private Wishlist wishlist;
+	//For Wishlist
+	private Movie wishlist[] = new Movie[20];
+	private int n;
+	private int front;
 	
 	public Customer(String name0, String email0, int card0) 
 	{
-		wishlist = new Wishlist(); //this is creating an empty wishlist that we will add movies into 
 		name = name0;
 		email = email0;
 		card = card0;
 
 		left = null; //nothing to start
-		right = null; 
+		right = null;
+		
+		n = 0;
+		front = 0;
 	}
-	
+	//------------------------------------------------
+	//For Identification
 	public String getName()
 	{
 		return name;
@@ -48,10 +56,87 @@ public class Customer
 		return card;
 	}
 
-	public Wishlist getWishlist() {
-		return wishlist
+	//-----------------------------------------------------
+	//For Wishlist
+	public Movie front()
+	{
+	//returns the first movie of wishlist
+	//check if empty and then check what is in the queue and return the front
+		if (n == 0)
+		{
+			System.out.println("You have an empty wishlist!");
+			return null;
+		}
+		else
+		{
+			System.out.println("Next to watch: " + wishlist[front].getTitle());
+			return wishlist[front];
+		}
 	}
-	// For BST:
+	
+	public void watchMovie()
+	{
+	//returns and removes the first movie of wishlist
+		//check if it is empty first
+		if (n == 0)
+		{
+			System.out.println("You have an empty wishlist!");
+		}
+		else
+		{
+			Movie temp = wishlist[front];
+			front = (front + 1) % 20;
+			n--;
+			System.out.println("You have watched " + temp.getTitle() + "!");
+		}
+	}
+	
+	public void addMovie(Movie x)
+	{
+	//x added as the last element
+	// we need to check if the wihlist is full first and then proceed:
+		if (n == 20)
+		{
+			System.out.println("You have a full wishlist!");
+		}
+		else
+		{
+			int tail = (front + n) % 20; //this would be the back of the queue
+			wishlist[tail] = x;
+			n++;
+			System.out.println(x.getTitle() + " has been added!");
+		}
+	}
+	
+	public boolean isWishlistEmpty()
+	{
+	//returns true if wishlist is empty and false if it is not
+		System.out.println("You have an empty wishlist!");
+		return n == 0;
+	}
+	
+	public void printWishlist()
+	//check if it is empty, if not keep going and print
+	{
+	// printWishlist method for Wishlist
+		int tail = (front + n) % 20;
+		//System.out.println(front);
+		//System.out.println(tail);
+		if (front <= tail)
+			for(int i = front; i < tail; i++) 
+				System.out.println(wishlist[i].getID());
+		else 
+		{
+			for(int i = front; i < 20; i++) 
+				System.out.println(wishlist[i].getID());          
+			for(int i = 0; i < tail; i++) 
+				System.out.println(wishlist[i].getID());
+		}          
+	}
+
+	
+	//-----------------------------------------------------
+	//For BST:
 	public void setRight(Customer right0)
 	{
 		right = right0;
